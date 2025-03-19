d5a70bf9af1e65795652cad5331151eaebd0459a
Catalina-S <catalina.sanchez-m@mail.escuelaing.edu.co>
2025-03-19 16:13:03 -0500

    cambio de contraseña

--- connection.py
+++ connection.py
@@ -3,7 +3,7 @@ from pymongo.server_api import ServerApi
 
 
 def connect_to_mongodb(db_name, collection_name):
-    uri = "mongodb+srv://catalinasanmu98765:<db_password>@cluster0.5goes.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    uri = "mongodb+srv://catalinasanmu98765:F0LmP240@cluster0.5goes.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     client = MongoClient(uri, server_api=ServerApi('1'))
     db = client[db_name]
     collection = db[collection_name]
