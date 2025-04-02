696b770c2c7442865d8fc704a6544463677a1884
Catalina-S <catalina.sanchez-m@mail.escuelaing.edu.co>
2025-04-02 10:36:02 -0500

    Update app1.py

--- app/app.py
+++ app/app.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI, HTTPException, Request
 import uvicorn
-from app.controlador.PatientCrud import GetPatientById,WritePatient
+from app.controlador.PatientCrud import GetPatientById,WritePatient,GetPatientByIdentifier
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://hl7-patient-catalina.onrender.com/"],  # Permitir solo este dominio
+    allow_origins=["*"],  # Permitir solo este dominio
     allow_credentials=True,
     allow_methods=["*"],  # Permitir todos los métodos (GET, POST, etc.)
     allow_headers=["*"],  # Permitir todos los encabezados
@@ -23,6 +23,17 @@ async def get_patient_by_id(patient_id: str):
     else:
         raise HTTPException(status_code=500, detail=f"Internal error. {status}")
 
+@app.get("/patient", response_model=dict)
+async def get_patient_by_id(system: str, value: str):
+    print("solicitud datos",system,value)
+    status,patient = GetPatientByIdentifier(system,value)
+    if status=='success':
+        return patient  # Return patient
+    elif status=='notFound':
+        raise HTTPException(status_code=204, detail="Patient not found")
+    else:
+        raise HTTPException(status_code=500, detail=f"Internal error. {status}")
+
 
 @app.post("/patient", response_model=dict)
 async def add_patient(request: Request):
@@ -33,6 +44,6 @@ async def add_patient(request: Request):
     else:
         raise HTTPException(status_code=500, detail=f"Validating error: {status}")
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
